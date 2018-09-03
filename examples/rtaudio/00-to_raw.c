786f548b88d55b287f79d523b8aa0620bbb186b0
tiagoft <tiagoft@gmail.com>
2018-09-03 11:50:02 -0300

    Adicionei funcionalidade para converter wav para raw-ascii

---
+++ examples/rtaudio/00-to_raw.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <stdint.h>
+
+
+int main() {
+  FILE *pipein, *wout;
+  int16_t buffer;
+  float output;
+
+  pipein = popen("ffmpeg -i assets/blip.wav -f s16le -ac 1 -ar 44100 -", "r");
+  wout = fopen("assets/blip.dat", "w");
+
+  int count, n=0;
+  while (1) {
+    count = fread(&buffer, 2, 1, pipein);
+    if (count != 1) break;
+    output = buffer / (float)32767; /* Scale INT output to a float in range [-1, 1] */
+    fprintf(wout, "%f\n", output);
+  }
+
+  fclose(pipein);
+  fclose(wout);
+  return 0;
+}
